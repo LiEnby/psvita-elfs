2eacf7365081bf12e2ffc951181a34836fef1bb0
Li <li@silica.codes>
2022-05-11 08:03:04 +1200

    Add fw 1.66 CEX

---
+++ vs0/data/external/webcore/mapview/libjs/google_maps_version_3_0.js
@@ -0,0 +1,19 @@
+
+
+window.google = window.google || {};
+google.maps = google.maps || {};
+(function() {
+  
+  function getScript(src) {
+    document.write('<' + 'script src="' + src + '"' +
+                   ' type="text/javascript"><' + '/script>');
+  }
+  
+  google.maps.Load = function(apiLoad) {
+    delete google.maps.Load;
+    apiLoad([,[[["http://mt0.google.com/vt?lyrs=m@132\u0026src=api\u0026hl=ja\u0026","http://mt1.google.com/vt?lyrs=m@132\u0026src=api\u0026hl=ja\u0026"],,"foo"],[["http://khm0.google.com/kh?v=68\u0026hl=ja\u0026","http://khm1.google.com/kh?v=68\u0026hl=ja\u0026"],,"foo",,1],[["http://mt0.google.com/vt?lyrs=h@132\u0026src=api\u0026hl=ja\u0026","http://mt1.google.com/vt?lyrs=h@132\u0026src=api\u0026hl=ja\u0026"],,"foo","imgtp=png32\u0026"],[["http://mt0.google.com/vt?lyrs=t@125,r@132\u0026src=api\u0026hl=ja\u0026","http://mt1.google.com/vt?lyrs=t@125,r@132\u0026src=api\u0026hl=ja\u0026"],,"foo"],,[[,0,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026"]],[,0,8,9,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1293600000]],[[354690000,1293600000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026"]],[,0,10,19,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.12\u0026hl=ja\u0026"]],[,3,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026"]],[,3,8,9,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1293600000]],[[354690000,1293600000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026"]],[,3,10,,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.12\u0026hl=ja\u0026"]]],[["http://cbk0.google.com/cbk?","http://cbk1.google.com/cbk?"],,"foo"],[["http://khmdb0.google.com/kh?v=31\u0026hl=ja\u0026","http://khmdb1.google.com/kh?v=31\u0026hl=ja\u0026"],,"foo"],[["http://mt0.google.com/mapslt?hl=ja\u0026","http://mt1.google.com/mapslt?hl=ja\u0026"],,"foo"],[["http://mt0.google.com/mapslt/ft?hl=ja\u0026","http://mt1.google.com/mapslt/ft?hl=ja\u0026"],,"foo"]],["ja","US",,,,"http://maps.google.com","http://maps.gstatic.com/intl/ja_ALL/mapfiles/","http://gg.google.com","https://maps-api-ssl.google.com"],["http://maps.gstatic.com/intl/ja_ALL/mapfiles/api-3/2/4a","3.2.4a"],[2824704147],1,,,,,], loadScriptTime);
+    
+  };
+  var loadScriptTime = (new Date).getTime();
+  getScript("http://maps.gstatic.com/intl/ja_ALL/mapfiles/api-3/2/4a/main.js");
+})();
